2cf8ff7fec6afac65d6071461a2a6f777868b6c3
Luis Ore Cardenas <luisore24@gmail.com>
2024-04-27 15:54:50 -0500

    update request mapping in tarifario controller

--- src/main/java/com/cibertec/autoparkback/controller/TarifarioController.java
+++ src/main/java/com/cibertec/autoparkback/controller/TarifarioController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/v1/api/tarifa")
+@RequestMapping("/v1/api/tarifario")
 public class TarifarioController {
 
     @Autowired
