ee62b14ad3f9df0f8199d0a42fbad6d681d83161
angelveliz1837 <99308699+angelveliz1837@users.noreply.github.com>
2024-04-29 10:45:36 -0500

    Add files via upload

---
+++ autopark-back-dev_veliz/AutoPark/src/main/java/com/cibertec/autopark/dtos/ClienteCreateDTO.java
@@ -0,0 +1,16 @@
+package com.cibertec.autopark.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+@Getter
+@Setter
+public class ClienteCreateDTO {
+    private String dni;
+    private String nombres;
+    private String apePaterno;
+    private String apeMaterno;
+    private Date fechaNacimiento;
+    private String email;
+}
