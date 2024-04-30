35f09c4a01ed02523c2478d08f5365edcd62820c
angelveliz1837 <99308699+angelveliz1837@users.noreply.github.com>
2024-04-29 21:22:44 -0500

    Delete autopark-back-dev_veliz/AutoPark directory

--- autopark-back-dev_veliz/AutoPark/src/main/java/com/cibertec/autopark/repository/MarcaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cibertec.autopark.repository;
-
-import com.cibertec.autopark.model.Marca;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MarcaRepository extends JpaRepository<Marca, Integer> {
-}
--- autopark-back-dev_veliz/AutoPark/src/main/java/com/cibertec/autopark/repository/TipoVehiculoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cibertec.autopark.repository;
-
-import com.cibertec.autopark.model.TipoVehiculo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TipoVehiculoRepository extends JpaRepository<TipoVehiculo, Integer> {
-}
